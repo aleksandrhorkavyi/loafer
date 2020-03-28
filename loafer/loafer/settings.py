b848df7bb8c98acd1707eda55aeac941f63c80bc
aleksandrhorkavyi <alexander.horkavyi@betterme.world>
2020-03-28 15:46:55 +0200

    fix

--- loafer/loafer/settings.py
+++ loafer/loafer/settings.py
@@ -81,21 +81,21 @@ WSGI_APPLICATION = 'loafer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': BASE_DIR + '/mysql.cnf',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': BASE_DIR + '/mysql.cnf',
+#         },
+#     }
+# }
 
 
 # Password validation
